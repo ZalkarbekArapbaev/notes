7bee906915c5c7043bb509af87fb7b782cdd2b26
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2022-12-21 17:21:57 +0600

    notes

---
+++ 1.Realm/ModelRealmSwift.swift
@@ -0,0 +1,13 @@
+//
+//  ModelRealmSwift.swift
+//  1.Realm
+//
+//  Created by zalkarbek on 13/11/22.
+//
+
+import Foundation
+import RealmSwift
+
+class ModelRealmSwift: Object {
+    @objc dynamic var name = ""
+}
